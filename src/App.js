9a9aa00489942deb0246d2e2d4f36c066d9de74a
Sachin Yadav <88329785+designed2code@users.noreply.github.com>
2022-01-28 16:39:25 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Users from "./components/Users";
+
+export default function App() {
+  return (
+    <div className="App" style={{textAlign:'center'}}>
+      <h1>Custom Hooks</h1>
+      <Users/>
+    </div>
+  );
+}
